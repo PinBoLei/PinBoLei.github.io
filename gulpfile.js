821eef2f5a1283a8cdc57daefac5ce87d77de90f
PinBoLei <hiliusir@163.com>
2018-12-03 12:41:02 +0800

    修改gulp配置

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
+//var imagemin = require('gulp-imagemin');
 // 压缩css文件
 gulp.task('minify-css', function() {
   return gulp.src('./public/**/*.css')
@@ -29,17 +29,17 @@ gulp.task('minify-js', function() {
         .pipe(gulp.dest('./public'));
 });
 // 压缩 public/demo 目录内图片
-gulp.task('minify-images', function() {
-    gulp.src('./public/demo/**/*.*')
-        .pipe(imagemin({
-           optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-           progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-           interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-           multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
-        }))
-        .pipe(gulp.dest('./public/uploads'));
-});
+// gulp.task('minify-images', function() {
+//     gulp.src('./public/demo/**/*.*')
+//         .pipe(imagemin({
+//            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+//            interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//            multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }))
+//         .pipe(gulp.dest('./public/uploads'));
+// });
 // 默认任务
 gulp.task('default', [
-  'minify-html','minify-css','minify-js','minify-images'
+  'minify-html','minify-css','minify-js'
 ]);
